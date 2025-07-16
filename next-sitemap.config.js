f9d1d489a26da41507a7a536aa4e7eef645ba99c
Rui Eduardo <turbid_smugger0i@icloud.com>
2025-07-16 17:52:34 +0200

    Adds next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://oftheuseless.com", // replace with your actual domain
+  generateRobotsTxt: true, // optional but recommended
+  generateIndexSitemap: true,
+};
